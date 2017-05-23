0255616e1f88f581352fd89d0acbeeec89b62176
Apatully <Apatully@gmail.com>
2017-05-23 19:49:44 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+	
+	
+	$('#modalShow').on('click', modalShow)
+	function modalShow() {
+	$('.modalWindow').show();	
+	}
+	$('#getPucture')on('click', addPucture);
+	function addPicture() {
+			var name = 
+			function category_validation(name) {
+			this.id_category = ;
+			this.title = name;
+			this.image = ;
+			this.data = ;
+			
+			}
+
+		var categoryValidation = new category_validation(name, );	
+	}	
+})
